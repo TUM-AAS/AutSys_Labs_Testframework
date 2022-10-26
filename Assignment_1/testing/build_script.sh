8bb46b45afd5b7857709d34d09282983c382e1e2
lina <lina@vanbruegge.de>
2022-10-26 14:10:05 +0200

    add autograding to testframework

--- Assignment_1/testing/build_script.sh
+++ Assignment_1/testing/build_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
 cp -r AutSys_Labs_Testframework/Assignment_1/testframework catkin_ws/src
+cp -r AutSys_Labs_Testframework/Assignment_1/testing/autograding.json .github/classroom
 ls catkin_ws/src
 cd catkin_ws
 catkin init
--- Assignment_2/testing/build_script.sh
+++ Assignment_2/testing/build_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
 cp -r AutSys_Labs_Testframework/Assignment_2/testframework catkin_ws/src
+cp AutSys_Labs_Testframework/Assignment_2/testing/autograding.json .github/classroom
 cd catkin_ws/src
 git clone https://github.com/ethz-asl/mav_comm.git
 cd ..
