f54157fd39f5fb7713f66b237324aca1ffbac7c3
lina <lina@vanbruegge.de>
2022-10-26 14:38:24 +0200

    change pipeline for autograding

--- Assignment_1/testing/build_script.sh
+++ Assignment_1/testing/build_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
 cp -r AutSys_Labs_Testframework/Assignment_1/testframework catkin_ws/src
-cp -r AutSys_Labs_Testframework/Assignment_1/testing/autograding.json .github/classroom
 ls catkin_ws/src
 cd catkin_ws
 catkin init
