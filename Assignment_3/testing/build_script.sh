f4e1223db20225e80db1a30761b04c79710d95b4
lina <lina@vanbruegge.de>
2022-11-03 13:30:01 +0100

    add mav trajectory generation to build script

--- Assignment_3/testing/build_script.sh
+++ Assignment_3/testing/build_script.sh
@@ -8,6 +8,7 @@ git clone https://github.com/ethz-asl/eigen_checks.git
 git clone https://github.com/catkin/catkin_simple.git
 git clone https://github.com/ethz-asl/glog_catkin.git
 git clone https://github.com/ethz-asl/nlopt.git
+git clone https://github.com/ethz-asl/mav_trajectory_generation.git
 cd ..
 catkin init
 catkin build -DTEST_FLAG=0
