1fe80a2534063e1abd62aea00230f9f54f3f6bd6
Lukas Pries <lukaspries@aol.com>
2024-11-05 12:51:40 +0100

    (update) build_script.sh | not cloning mav_comm manually since it is already integrated in src as a submodule

--- Assignment_2/testing/build_script.sh
+++ Assignment_2/testing/build_script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
 cp -r AutSys_Labs_Testframework/Assignment_2/testframework catkin_ws/src
-cd catkin_ws/src
-git clone https://github.com/ethz-asl/mav_comm.git
-cd ..
+cd catkin_ws
 catkin init
 catkin build -DTEST_FLAG=0
