daaa8c82e1cdc4ece651e041ddccd34eadb18e8f
Lukas Pries <lukaspries@aol.com>
2024-11-05 13:36:07 +0100

    (add) required python dependencies

--- Assignment_2/testing/build_script.sh
+++ Assignment_2/testing/build_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
-pip install em==3.3.4
+pip install empy==3.3.4
 cp -r AutSys_Labs_Testframework/Assignment_2/testframework catkin_ws/src
 cd catkin_ws
 catkin init
