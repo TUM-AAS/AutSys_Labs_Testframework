ba5a2147698b34c3fb5675287cd3c99200624c67
Lukas Pries <lukaspries@aol.com>
2024-12-02 14:46:45 +0100

    remove cloning packagages (already included in ws)

--- Assignment_3/testing/build_script.sh
+++ Assignment_3/testing/build_script.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 source /opt/ros/noetic/setup.bash
 cp -r AutSys_Labs_Testframework/Assignment_3/testframework catkin_ws/src
-cd catkin_ws/src
-git clone https://github.com/ethz-asl/mav_comm.git
-git clone https://github.com/ethz-asl/eigen_catkin.git
-git clone https://github.com/ethz-asl/eigen_checks.git
-git clone https://github.com/catkin/catkin_simple.git
-git clone https://github.com/ethz-asl/glog_catkin.git
-git clone https://github.com/ethz-asl/nlopt.git
-git clone https://github.com/ethz-asl/mav_trajectory_generation.git
-git clone https://github.com/ethz-asl/yaml_cpp_catkin.git
-cd ..
+cd catkin_ws
 catkin init
 catkin build -DTEST_FLAG=0
